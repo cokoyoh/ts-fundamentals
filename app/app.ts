cac7b177d66d5cea0b3dc48c92fa1adda59fdcb4
cokoyoh <charlesokoyoh@gmail.com>
2020-07-31 11:12:24 +0300

    Using type anotations and type inferences

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,17 @@
 function startGame() {
     // starting a new game
+
+    const playerName: string = 'Jane Doe';
+    logPlayer(playerName);
+
     let messageElement = document.getElementById('messages');
 
     if (messageElement) messageElement.innerText = 'Welcome to MultiMath! Staring a new game...';
 }
 
-let startGameElement = document.getElementById('startGame');
+function logPlayer(name) {
+    console.log(`'New game starting for player: ${name}`);
+}
 
+let startGameElement = document.getElementById('startGame');
 if (startGameElement) startGameElement.addEventListener('click', startGame);

5a9ad41c031961e134764afc61412f3e8fc6fda5
cokoyoh <charlesokoyoh@gmail.com>
2020-08-01 11:01:12 +0300

    Taking advantage of typescript functions with correct types

--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,7 @@ function startGame() {
     logPlayer(playerName);
 
     postScore(100, playerName);
+    postScore(-5, playerName);
 }
 
 function logPlayer(name: string = 'MultiMath Player'): void {
@@ -22,11 +23,22 @@ function getInputValue(elementId: string): string | undefined {
 }
 
 function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    let logger: (value: string) => void;
+
+
+    if (score < 0) {
+        logger = logError;
+    } else {
+        logger = logMessage;
+    }
+
     const scoreElement: HTMLElement | null = document.getElementById('postedScores');
 
     if (scoreElement) {
         scoreElement.innerText = `${score} - ${playerName}`;
     }
+
+    logger(`Score: ${score}`);
 }
 
 let startGameElement = document.getElementById('startGame');
@@ -35,4 +47,4 @@ if (startGameElement) startGameElement.addEventListener('click', startGame);
 
 const logMessage = (message: string) => console.log(message);
 
-logMessage('Welcome to MultiMath');
+const logError = (error: string) => console.error(error);

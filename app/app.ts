4f20d8c88633112b23db0e8134f82ecc398c956c
cokoyoh <charlesokoyoh@gmail.com>
2020-08-01 10:40:26 +0300

    Add type annnotations to functions

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,32 @@
 function startGame() {
     // starting a new game
 
-    let playerName: string = 'Jane Doe';
+    let playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName);
 
-    let messageElement = document.getElementById('messages');
-
-    if (messageElement) messageElement.innerText = 'Welcome to MultiMath! Staring a new game...';
+    postScore(100, playerName);
 }
 
-function logPlayer(name: string): void {
+function logPlayer(name: string = 'MultiMath Player'): void {
     console.log(`'New game starting for player: ${name}`);
+}
+
+function getInputValue(elementId: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+
+    if (inputElement.value === '') {
+        return undefined;
+    }
+
+    return inputElement.value;
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+
+    if (scoreElement) {
+        scoreElement.innerText = `${score} - ${playerName}`;
+    }
 }
 
 let startGameElement = document.getElementById('startGame');
